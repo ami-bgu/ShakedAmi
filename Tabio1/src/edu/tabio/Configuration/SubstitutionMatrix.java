c22b40564232414651ff9012767c1de6be912b4b
Ami <amioren10@gmail.com>
2015-04-02 12:44:41 +0300

    done

--- Tabio1/src/edu/tabio/Configuration/SubstitutionMatrix.java
+++ Tabio1/src/edu/tabio/Configuration/SubstitutionMatrix.java
@@ -2,20 +2,13 @@ package edu.tabio.Configuration;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SubstitutionMatrix {
 
-	
-	private final int A =  0;
-	private final int T =  1;
-	private final int G =  2;
-	private final int C =  3;
-	private final int U =  4;
-	private final int N =  5;
-	private final int BLANK =  6;
-	
-	
+
 	public int getAffineGap_A() {
 		return affineGap_A;
 	}
@@ -24,21 +17,18 @@ public class SubstitutionMatrix {
 		return affineGap_B;
 	}
 
-	private int affineGap_A = 0;	//starting a new gap
-	private int affineGap_B = 0;	//elongation of gap
+	private int affineGap_A = 1;	//starting a new gap
+	private int affineGap_B = 1;	//elongation of gap
 	
-	private int[][] mat = new int[7][7]; 
+	private int[][] mat = null;
 	
 	
-	
-
 	public SubstitutionMatrix(String filename) {
 
 		boolean didIReadSubstitutionMatrix = false;
 		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
 			String line;
 			while ((line = br.readLine()) != null) {
-				if (line.charAt(0) == '#')		continue;
 				if (!didIReadSubstitutionMatrix)
 				{
 					readSubsMatrix(br);
@@ -46,6 +36,8 @@ public class SubstitutionMatrix {
 				}
 				else
 				{
+					if (line.charAt(0) == '#')		continue;
+
 					String[] arrA = line.split(" +");
 					affineGap_A = Integer.parseInt(arrA[1]);
 					
@@ -60,61 +52,95 @@ public class SubstitutionMatrix {
 		}
 	}
 	
-	
-	private int letterToIndex(char letter)
-	{
-		switch (letter) {
-			case 'A':	return A;
-			case 'T':	return T;
-			case 'C':	return C;
-			case 'G':	return G;
-			case 'U':	return U;
-			case ' ':	return BLANK;
-			default:	return N;
+	private int getIndexByLetter(char c, boolean isHorizontal){
+		if (c=='_' || c==' ') c='*';
+		
+		char[] array = null;
+		
+		array = isHorizontal?horizontalLetters:verticalLetters;
+		
+		for (int i = 0; i < array.length; i++) {
+			if (array[i]==c)	return i;
 		}
+		c='N';
+		for (int i = 0; i < array.length; i++) {
+			if (array[i]==c)	return i;
+		}
+		System.err.println("Cant find letter in substitution matrix");
+		System.exit(0);
+		return -1;
 	}
 	
 	public int score(char letter1, char letter2) {
-		return mat[letterToIndex(letter1)][letterToIndex(letter2)];
+		return mat[getIndexByLetter(letter1,false)][getIndexByLetter(letter2,true)];
 	}
 	
-//	private int score(int a, int b) {
-//		return mat[a][b];
-//	}
-	
+
+	private char horizontalLetters[];
+	private char verticalLetters[];
+
 	
 	private void readSubsMatrix(BufferedReader br)	throws IOException
 	{
 		String line;
-		for (int i = 0; i < 7; i++) {
-			line = br.readLine();
-			String[] arr = line.split(" +");
-			for (int j = 1; j < arr.length; j++) {
-				mat[i][j-1] = Integer.parseInt(arr[j]);
+
+		while ((line = br.readLine()) != null && line.charAt(0) == '#') {}
+
+		String[] arr = line.trim().split(" +");
+		horizontalLetters = new char[arr.length];
+		for (int i = 0; i < horizontalLetters.length; i++) {
+			horizontalLetters[i] = arr[i].charAt(0);
+		}
+		List<String> rows = new ArrayList<>();
+		
+		while ((line = br.readLine()) != null && line.charAt(0) != '#') {
+			rows.add(line);
+		}
+
+		verticalLetters = new char[rows.size()];
+
+		mat = new int[verticalLetters.length][horizontalLetters.length];
+		
+		for (int i = 0; i < verticalLetters.length; i++) {
+			arr = rows.get(i).trim().split(" +");
+			verticalLetters[i] = arr[0].charAt(0);
+			for (int j = 0; j < horizontalLetters.length; j++) {
+				mat[i][j] = Integer.parseInt(arr[j+1]);
 			}
 		}
+		
 	}
 	
 	@Override
 	public String toString() {
 		//System.out.println(affineGap_B);
 		StringBuilder builder = new StringBuilder();
-		for (int i = 0; i < 7; i++) {
-			for (int j = 0; j < 7; j++) {
+
+		builder.append("First Row is: ");
+		for (char c : horizontalLetters) {
+			builder.append(c+" ");
+		}
+		builder.append("\nFirst Col is: ");
+		for (char c : verticalLetters) {
+			builder.append(c+" ");
+		}
+		builder.append("\n-----------------------------------------------------\n");
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[0].length; j++) {
 				builder.append(mat[i][j]+"\t");
 			}
 			builder.append("\n");
 		}
+		builder.append("-----------------------------------------------------");
 		return builder.toString();
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SubstitutionMatrix matrix  = new SubstitutionMatrix("Score.matrix");
+		SubstitutionMatrix matrix  = new SubstitutionMatrix("Fake.matrix");
 		System.out.println(matrix);
-		
-		
-		
+		System.out.println(matrix.score('A', 'A'));
 	}
+
 
 }
